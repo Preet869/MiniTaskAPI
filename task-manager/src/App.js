543c4c4057e1a87568eb6a758b50f8fcee40ae4d
Preet Dhillon <Preet_231@icloud.com>
2025-03-22 16:05:07 +1100

    Added Input box

--- task-manager/src/App.js
+++ task-manager/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useState } from "react";
+
 
 function App() {
+   const[title, setTitle] = useState("");
+   const[tasks, setTasks] = useState([]);
+
   return (
     <div>
       <h1><center>Task Manager</center></h1>
+      <label>
+          Task Input
+          <input value={title} onChange={(e) => setTitle(e.target.value)}/>
+      </label>
     </div>
   );
 }
