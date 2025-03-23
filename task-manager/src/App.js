0a058292e48c7e3ee6143282aa2634c0dc9d4be2
Preet <preet_231@icloud.com>
2025-03-23 21:16:26 +1100

    Button and Text box logic

--- task-manager/src/App.js
+++ task-manager/src/App.js
@@ -1,18 +1,27 @@
-import React from 'react';
-import { useState } from "react";
-
+import React, { useState } from 'react';
 
 function App() {
-   const[title, setTitle] = useState("");
-   const[tasks, setTasks] = useState([]);
+  const [title, setTitle] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  function addTask() {
+    const newTask = {
+      id: tasks.length + 1,
+      title: title,
+      completed: false
+    };
+    setTasks([...tasks, newTask]);
+    setTitle("");
+  }
 
   return (
     <div>
       <h1><center>Task Manager</center></h1>
       <label>
-          Task Input
-          <input value={title} onChange={(e) => setTitle(e.target.value)}/>
+        Task Input
+        <input value={title} onChange={(e) => setTitle(e.target.value)} />
       </label>
+      <button onClick={addTask}>Add Task</button>
     </div>
   );
 }
